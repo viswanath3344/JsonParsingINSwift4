9d5a6cde4577e970422c865f4af3efc840a4de9c
Viswa <viswanath3344@gmail.com>
2018-06-20 16:17:59 +0530

    Initial Commit

--- JsonParsingINSwift4/ViewController.swift
+++ JsonParsingINSwift4/ViewController.swift
@@ -10,8 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    struct WebSiteDescription:Decodable {
+        let name:String?
+        let description:String?
+        let courses: [Courses]
+    }
+    
+    struct Courses:Decodable {
+        let id : Int?
+        let name: String?
+        let link:String?
+        //let number_of_lessons: Int?  // With out keyDecodingStrategy
+        let numberOfLessons :Int? //with  keyDecodingStrategy = .convertFromSnakeCase  replace _ with Required.
+    }
+    
+    private func loadJsonDataFromUrl()
+    {
+        let jsonUrl  = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
+        
+        guard let url  = URL(string: jsonUrl) else
+        {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            
+            guard let data = data else
+            {
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase  // replace _
+                let webSiteDescriptionObject = try decoder.decode(WebSiteDescription.self, from: data)
+                print(webSiteDescriptionObject.courses.last!.numberOfLessons! )
+                
+            }
+            catch let jsonError
+            {
+                print("JsonParsing Error \(jsonError)")
+                
+            }
+            
+        }.resume()
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadJsonDataFromUrl()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
