18cbde43e70187482f83f53d790bbfd2bea6e73f
Viswa <viswanath3344@gmail.com>
2018-06-20 15:23:05 +0530

    Initial Commit

---
+++ JsonParsingINSwift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsonParsingINSwift4
+//
+//  Created by Ming-En Liu on 20/06/18.
+//  Copyright © 2018 Vedas labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
